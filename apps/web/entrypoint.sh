5c3d5f0cca19ff58151b78f4e11af122d71b7974
Sagar <sagar@aryal.me>
2025-04-04 13:48:25 +0200

    Move docker to run prod env

--- apps/web/entrypoint.sh
+++ apps/web/entrypoint.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
 
-pnpm install
-pnpm prisma migrate dev
-pnpm run dev 
+if [ "$NODE_ENV" = "production" ]; then
+  echo "Installing production dependencies..."
+  pnpm install
+  pnpm prisma generate
+  pnpm prisma migrate deploy
+  pnpm run build
+  pnpm run start
+else
+  pnpm install
+  pnpm prisma migrate dev
+  pnpm run dev
+fi
